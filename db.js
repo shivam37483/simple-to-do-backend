ca20d3ffcc8f02fa6c0946bc6a67d1ef234a84d5
Shivam Vats <shivamvats@Shivams-MacBook-Air.local>
2025-08-05 02:08:41 +0530

    Remove redundant code

--- db.js
+++ db.js
@@ -22,28 +22,3 @@ module.exports = {
     userModel,
     todoModel
 }
-
-// const mongoose = require("mongoose");
-
-// const Schema = mongoose.Schema;
-// const ObjectId = Schema.ObjectId;
-
-// const User = new Schema({
-//   name: String,
-//   email: String,
-//   password: String
-// });
-
-// const Todo = new Schema({
-//     userId: ObjectId,
-//     title: String,
-//     done: Boolean
-// });
-
-// const UserModel = mongoose.model('users', User);
-// const TodoModel = mongoose.model('todos', Todo);
-
-// module.exports = {
-//     UserModel,
-//     TodoModel
-// }
